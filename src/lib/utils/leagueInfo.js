01c4b0e02040c5883f6e2c6e38eb925d4966b8a7
Nulap <iamjorgan@gmail.com>
2022-01-12 19:09:52 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "650171853414981633"; // your league ID
+export const leagueID = "784483220396883968"; // your league ID
 export const leagueName = "Incon Keeper League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
